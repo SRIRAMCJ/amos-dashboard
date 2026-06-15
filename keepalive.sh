6588decb7add8b3ef92aa443244d2dbe6c34e032
Z User <z@container>
2026-06-15 06:05:54 +0000

    9a0c333e-efbb-48ca-b2ff-9602084c8a69

---
+++ keepalive.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /home/z/my-project
+while true; do
+  echo "[$(date)] Starting dev server..." >> /home/z/my-project/dev.log
+  npx next dev -p 3000 >> /home/z/my-project/dev.log 2>&1
+  echo "[$(date)] Server exited, restarting in 3s..." >> /home/z/my-project/dev.log
+  sleep 3
+done
