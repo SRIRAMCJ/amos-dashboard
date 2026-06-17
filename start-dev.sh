00718ed054432a73dd5d28a546fe9d76eb6da5a8
Z User <z@container>
2026-06-17 07:49:23 +0000

    6e6a471d-b767-496b-ad93-bbe68d26a005

--- start-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-unset DATABASE_URL
-unset DIRECT_URL
-export DATABASE_URL="postgresql://neondb_owner:npg_ExbWfuIykY96@ep-muddy-shadow-aonbdse1-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-export DIRECT_URL="postgresql://neondb_owner:npg_ExbWfuIykY96@ep-muddy-shadow-aonbdse1.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-cd /home/z/my-project
-exec npx next dev -p 3000
